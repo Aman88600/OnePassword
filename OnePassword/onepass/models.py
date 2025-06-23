7a9e6664d5593a8c29f85cf10ce3fe40749a592f
Aman88600 <amanbasoya02@gmail.com>
2025-06-23 23:34:04 +0530

    'signup_page'

--- OnePassword/onepass/models.py
+++ OnePassword/onepass/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class onepass_users(models.Model):
+    user_name = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"user_name = {self.user_name} and password = {self.password}"
--- OnePassword/onepass/views.py
+++ OnePassword/onepass/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-
+from .models import onepass_users
 
 # Create your views here.
 def index(request):
     return render(request, "onepass/index.html")
 
 def login_page(request):
+
     return render(request, "onepass/login_page.html")
 
 def signup_page(request):
+    if request.method == "POST":
+        user_name = request.POST.get("user_name")
+        password = request.POST.get("password")
+        new_user = onepass_users(user_name=user_name, password=password)
+        new_user.save()
+        return render(request, "onepass/login_page.html")
+    # elif request.method == "GET":
     return render(request, "onepass/signup_page.html")
