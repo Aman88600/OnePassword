6678ed73e740f4f6cce7047a3805e0dc72913410
Aman88600 <amanbasoya02@gmail.com>
2025-06-23 23:56:19 +0530

    'text_area'

--- OnePassword/onepass/admin.py
+++ OnePassword/onepass/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import onepass_users
+from .models import onepass_users, onepass_text
 # Register your models here.
 admin.site.register(onepass_users)
+admin.site.register(onepass_text)
--- OnePassword/onepass/models.py
+++ OnePassword/onepass/models.py
@@ -7,3 +7,9 @@ class onepass_users(models.Model):
 
     def __str__(self):
         return f"user_name = {self.user_name} and password = {self.password}"
+
+class onepass_text(models.Model):
+    user_name = models.CharField(max_length=64, default="<user_name>")
+    user_data = models.TextField(default="<user_data>")
+    def __str__(self):
+        return f"user_data = {self.user_data}"
